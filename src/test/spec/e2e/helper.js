f8b66e9ce46fbe698ec33f1b4f22f6381d60d563
Chong, Michael <michong@ci.com>
2016-07-28 15:36:29 -0400

    Made window title same as "app title - page name" as the page name appears in the desktop nav.
    Changed footer so it doesn't disappear on the mobile view when the slide out menu opens.
    
    Added helper functions to test the main page and footer.

--- src/test/spec/e2e/helper.js
+++ src/test/spec/e2e/helper.js
@@ -1,8 +1,128 @@
 var defaultTimeout = 1000;
 var browserSync = element(by.id('__bs_notify__'));
 var _ = require('lodash');
+
+
+
+//This function loads the specified url into a window with the specified dimensions and waits for angular to finish rendering.
+//URL = the path to the page you want to load
+//width = the width of the browser window
+//height = the height of the browser window
+function loadPageWait(URL, width, height) {
+    browser.get(URL);
+    if (typeof width == "number" && typeof height == "number") {
+        browser.driver.manage().window().setSize(width, height);
+    }
+    browser.waitForAngular();
+}
+
+function footer() {
+    var f = {};
+    f.footer = element(by.id('footer'));
+    f.expected = {
+        text: '©2016 Name of Company',
+        textVisibility: true,
+        logoVisibility: false,
+        mobileLogoVisibility: false
+    };
+    console.log('f.expected: ', f.expected);
+    f.logo = element(by.css("#cilogo img[src='./assets/images/logos.svg']"));
+    f.mobileLogo = element(by.css("#nav img[src='./assets/images/logos.svg']"));
+
+    f.checkText = function(visibility, text) {
+
+        if (visibility) {
+            f.expected.textVisibility = visibility;
+        }
+        if (text) {
+            f.expected.text = text;
+        }
+        expect(f.footer.isDisplayed()).toEqual(f.expected.textVisibility);
+        expect(f.footer.isPresent()).toEqual(false);
+        expect(f.footer.getText()).toEqual(f.expected.text);
+    }
+
+    f.checkLogoVisibility = function(logoVisibility) {
+        if (typeof logoVisibility == 'boolean') {
+            f.expected.logoVisibility = logoVisibility;
+        }
+        expect(f.logo.isDisplayed()).toBe(f.expected.logoVisibility);
+    }
+
+    f.checkMobileLogoVisibility = function(mobileLogoVisibility) {
+        if (typeof mobileLogoVisibility == 'boolean') {
+            f.expected.mobileLogoVisibility = mobileLogoVisibility;
+        }
+        expect(f.mobileLogo.isDisplayed()).toBe(f.expected.mobileLogoVisibility);
+    }
+    return f;
+}
+
+//The common properties that all pages will have
+function basePage() {
+    var basePage = {};
+    basePage.title = element(by.binding('Head.page.title'));
+    basePage.windowTitle = browser.getTitle();
+    basePage.content = element(by.id('content'));
+    basePage.expected = {
+        title:'',
+        content:''
+    };
+    basePage.checkMobileTitle = function() {
+        //every mobile page should have a title
+        expect(basePage.title.isDisplayed()).toEqual(true);
+        expect(basePage.title.getText()).toBe(basePage.expected.title);
+    }
+    basePage.checkMobileContent = function() {
+        //every mobile page should have content
+        expect(basePage.content.isDisplayed()).toEqual(true);
+        expect(basePage.content.getText()).toBe(basePage.expected.content);
+    }
+    return basePage;
+}
+
+function page1Template(expectedTitle, expectedContent) {
+    var p = basePage();
+    p.expected.title = expectedTitle;
+    p.expected.content = expectedContent;
+    //add additional functions/properties that are specific to page1
+    return p;
+}
+
+function page2Template(expectedTitle, expectedContent) {
+    var p = basePage();
+    p.expected.title = expectedTitle;
+    p.expected.content = expectedContent;
+    //add additional functions/properties that are specific to page2
+    return p;
+}
+
+function page3Template(expectedTitle, expectedContent) {
+    var p = basePage();
+    p.expected.title = expectedTitle;
+    p.expected.content = expectedContent;
+    //add additional functions/properties that are specific to page3
+    return p;
+}
+
+function page4Template(expectedTitle, expectedContent) {
+    var p = basePage();
+    p.expected.title = expectedTitle;
+    p.expected.content = expectedContent;
+    //add additional functions/properties that are specific to page4
+    return p;
+}
+
+function page5Template(expectedTitle, expectedContent) {
+    var p = basePage();
+    p.expected.title = expectedTitle;
+    p.expected.content = expectedContent;
+    //add additional functions/properties that are specific to page5
+    return p;
+}
+
 function mobileNavBar() {
-    var mnb = this;
+    var mnb = {};
     mnb.container = element(by.id('nav'));
     mnb.hamburger = $("#drawerlink");
     mnb.links = element.all(by.css('#nav li a'));
@@ -25,17 +145,14 @@ function mobileNavBar() {
                 browser.wait(protractor.ExpectedConditions.stalenessOf(browserSync) || protractor.ExpectedConditions.invisibilityOf(browserSync), defaultTimeout);
                 $("#drawerlink").click();
                 browser.wait(protractor.ExpectedConditions.visibilityOf(mnb.links.get(0)), defaultTimeout);
-                console.log('shown');
             } else {
                 //browser sync sometimes covers hamburger so we have to wait for it to go away before we can click on the hamburger
                 browser.wait(protractor.ExpectedConditions.stalenessOf(browserSync) || protractor.ExpectedConditions.invisibilityOf(browserSync), defaultTimeout);
                 $("#drawerlink").click();
                 browser.wait(protractor.ExpectedConditions.invisibilityOf(mnb.links.get(0)), defaultTimeout);
-                console.log('hidden');
             }
         });
     };
-
     /*
         links is an array of objects, where each object is of the form:
         {
@@ -56,10 +173,17 @@ function mobileNavBar() {
             }
         });
     };
-    return this;
+    return mnb;
 }
 module.exports = {
     mobileNavBar: mobileNavBar(),
     browserSync: browserSync,
-    defaultTimeout: defaultTimeout
+    defaultTimeout: defaultTimeout,
+    page1Template: page1Template,
+    page2Template: page2Template,
+    page3Template: page3Template,
+    page4Template: page4Template,
+    page5Template: page5Template,
+     loadPageWait: loadPageWait,
+    footer: footer()
 };
--- src/test/spec/e2e/homepage.spec.js
+++ src/test/spec/e2e/homepage.spec.js
@@ -1,28 +1,33 @@
 var helper = require('./helper.js');
 var _ = require('lodash');
 var mnb = helper.mobileNavBar;
+var page1Template = helper.page1Template;
+var page2Template = helper.page2Template;
+var page3Template = helper.page3Template;
+var page4Template = helper.page4Template;
+var page5Template = helper.page5Template;
+var loadPageWait = helper.loadPageWait;
+var footer = helper.footer;
+
 describe('advisorLocator mobile Navigation', function() {
     it('should be able to go to page 1', function() {
+        //Navigate to page 1 of the app, resize the window to force the mobile view, and wait for angular to finish.
+        /*
         browser.get('http://localhost:3000/#/en/advisorLocator/page1');
         browser.driver.manage().window().setSize(500, 500);
-        browser.waitForAngular();
+        browser.waitForAngular();*/
+        loadPageWait('http://localhost:3000/#/en/advisorLocator/page1', 500, 500);
 
-        expect(element(by.binding('Head.page.title')).isDisplayed()).toEqual(true);
-        expect(element(by.binding('Head.page.title')).getText()).toBe('Page 1 Title');
-        expect($("#content").getText()).toEqual('Temporary page 1 content\n\n\nbottom');
-        expect($("#content").isDisplayed()).toEqual(true);
-        expect($("#footer").getText()).toEqual('©2016 Name of Company');
-        expect($("#footer").isDisplayed()).toEqual(true);
-        var mypic = element(by.css("img[src='./assets/images/logos.svg']"));
-        expect(mypic.isDisplayed()).toBe(false);
+
+        //Make sure the page content is visible and what we expect it to be
+        var page1 = page1Template('Page 1 Title','Temporary page 1 content\n\n\nbottom');
+        page1.checkMobileTitle();
+        page1.checkMobileContent();
+        footer.checkText();
+        footer.checkLogoVisibility();
+        footer.checkMobileLogoVisibility();
+
         expect($("#drawerlink").isDisplayed()).toEqual(true);
-
-        //browser.wait(protractor.expectedConditions.elementIsNotVisible($('__bs_notify__')), 5000, 'Element not clickable');
-        /*
-        browser.wait(protractor.ExpectedConditions.stalenessOf($('#__bs_notify__')) || protractor.ExpectedConditions.invisibilityOf($('#__bs_notify__')), 5000);
-        $("#drawerlink").click();
-        browser.wait(protractor.ExpectedConditions.visibilityOf($("#nav li")), 5000);
-        */
 
 
         var expectedLinks = [
@@ -40,9 +45,16 @@ describe('advisorLocator mobile Navigation', function() {
         ];
         //mobile nav is not open, links should be on the page, but not visible
         mnb.verifyLinks(expectedLinks);
+        //verify that we can't see mobile nav logo when mobile nav is closed
         expect(mnb.logo.isDisplayed()).toEqual(false);
         //open mobile navbar
         mnb.clickHamburger();
+
+
+        footer.checkText(true);
+        footer.checkLogoVisibility();
+        footer.checkMobileLogoVisibility(true);
+
         //now we should expect the mobile navbar links to be visible
         var drawerNavItems = element.all(by.css('#nav li a'));
         _.forEach(expectedLinks, function(value, index){
@@ -50,57 +62,22 @@ describe('advisorLocator mobile Navigation', function() {
         });
         //verify navbar.
         mnb.verifyLinks(expectedLinks);
+        //verify that we can see the mobile nav logo when the nav is open
         expect(mnb.logo.isDisplayed()).toEqual(true);
+        //close navbar
+        mnb.clickHamburger();
 
-        /*
-        expect(drawerNavItems.get(0).isDisplayed()).toEqual(true);
-        expect(drawerNavItems.get(0).getText()).toEqual('Français');
+        footer.checkText();
+        footer.checkLogoVisibility();
+        footer.checkMobileLogoVisibility(false);
 
-        expect(drawerNavItems.get(1).isDisplayed()).toEqual(true);
-        expect(drawerNavItems.get(1).getText()).toEqual('Page 1 Title');
-        expect(drawerNavItems.get(2).isDisplayed()).toEqual(true);
-        expect(drawerNavItems.get(2).getText()).toEqual('Page 2 Title');
-        expect(drawerNavItems.get(3).isDisplayed()).toEqual(true);
-        expect(drawerNavItems.get(3).getText()).toEqual('Page 3 Title');
-        expect(drawerNavItems.get(4).isDisplayed()).toEqual(true);
-        expect(drawerNavItems.get(4).getText()).toEqual('Page 4 Title');
-        expect(drawerNavItems.get(5).isDisplayed()).toEqual(true);
-        expect(drawerNavItems.get(5).getText()).toEqual('Page 5 Title');
-        expect(drawerNavItems.get(6).isDisplayed()).toEqual(true);
-        expect(drawerNavItems.get(6).getText()).toEqual('Help');
-
-        //var drawerAnchors = element.all($('#nav li a'));
-
-        expect(drawerNavItems.get(7).isDisplayed()).toEqual(true);
-        expect(drawerNavItems.get(7).getText()).toEqual('Link 1');
-        expect(drawerNavItems.get(7).getAttribute('href')).toEqual('https://www.google.ca/');
-        expect(drawerNavItems.get(8).isDisplayed()).toEqual(true);
-        expect(drawerNavItems.get(8).getText()).toEqual('Link 2');
-        expect(drawerNavItems.get(8).getAttribute('href')).toEqual('https://www.yahoo.ca/');
-        expect(drawerNavItems.get(9).isDisplayed()).toEqual(true);
-        expect(drawerNavItems.get(9).getText()).toEqual('Link 3');
-        expect(drawerNavItems.get(9).getAttribute('href')).toEqual('https://www.ci.com/');
-        expect(drawerNavItems.get(10).isDisplayed()).toEqual(true);
-        expect(drawerNavItems.get(10).getText()).toEqual('Link 4');
-        expect(drawerNavItems.get(10).getAttribute('href')).toEqual('https://www.assanteservices.com/aiol/#/en/aio/landing/');
-        */
-
-        /*
-        expect(element(by.binding('pages.page1.title')).isDisplayed()).toEqual(true);
-        expect(element(by.binding('pages.page2.title')).isDisplayed()).toEqual(true);
-        expect(element(by.binding('pages.page3.title')).isDisplayed()).toEqual(true);
-        expect(element(by.binding('pages.page4.title')).isDisplayed()).toEqual(true);
-        expect(element(by.binding('pages.page5.title')).isDisplayed()).toEqual(true);
-        */
-
-        /* check if image exists before running some other tests
-        browser.isElementPresent(mypic).then(function (result) {
-            if(result){
-                //  DO STUFF
-                console.log('my Stuff: ', result);
-            }
+        _.forEach(expectedLinks, function(value, index){
+            value.visibility = false;
         });
-        */
+        //verify navbar links and logo are hidden again
+        mnb.verifyLinks(expectedLinks);
+        expect(mnb.logo.isDisplayed()).toEqual(false);
+
     });
 /*
     it('should be able to go to page 2', function() {
