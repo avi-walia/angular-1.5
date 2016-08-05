4ff8d873d395b883bd4c42be23b4d55c1d4576e2
Roslik, Dziana <droslik@ci.com>
2016-08-05 15:31:18 -0400

    removed idle module

--- src/app/bootstrap/index.module.js
+++ src/app/bootstrap/index.module.js
@@ -2,14 +2,14 @@
     'use strict';
 
     //---------------- CORE ---------------- //
-    angular.module('advisorLocator.core.main', ['ngIdle', 'advisorLocator.core.server', 'advisorLocator.utils', 'ui.bootstrap', 'ng.deviceDetector', 'headroom']);
+    angular.module('advisorLocator.core.main', ['advisorLocator.core.server', 'advisorLocator.utils', 'ui.bootstrap', 'ng.deviceDetector', 'headroom']);
     angular.module('advisorLocator.core.cache', ['advisorLocator.core.server']);
     angular.module('advisorLocator.core.server', ['advisorLocator.core.cache']);
 
     //---------------- FEATURES ---------------- //
 
     //---------------- UTILITIES ---------------- //
-    angular.module('advisorLocator.utils', ['matchMedia', 'ngIdle', 'advisorLocator.core.server', 'ui.bootstrap', 'ng.deviceDetector', 'headroom']);
+    angular.module('advisorLocator.utils', ['matchMedia', 'advisorLocator.core.server', 'ui.bootstrap', 'ng.deviceDetector', 'headroom']);
 
     //---------------- MAIN ---------------- //
     angular.module('advisorLocator', [
