6270c0ced800214640bbe7eca3fc5c0e2cde77be
Chong, Michael <michong@ci.com>
2016-08-17 14:35:20 -0400

    WIP - beginning to work on search by name.

--- src/app/features/components/advisorList/advisorList.component.js
+++ src/app/features/components/advisorList/advisorList.component.js
@@ -15,14 +15,44 @@
     advisorListCtrl.$inject = [
 
         'pageStateResolver',
-        'detectMobile'
+        'detectMobile',
+        'server',
+        'BASE_URL',
+        'ENDPOINT_URI',
+        'dataCacheLocalStorage'
     ];
     /* @ngInject */
-    function advisorListCtrl( pageStateResolver, detectMobile
+    function advisorListCtrl(
+        pageStateResolver,
+        detectMobile,
+        server,
+        BASE_URL,
+        ENDPOINT_URI,
+        dataCacheLocalStorage
     ) {
         var vm = this;
         vm.pageStateResolver = pageStateResolver;
         vm.detectMobile = detectMobile;
+        function init() {
+            vm.advisors = [];
+            vm.isLoading = true;
+            server.get(BASE_URL + ENDPOINT_URI + '/advisors', false, 'localStorage', false).then(function(data) {
+                console.log('data1123: ', data);
+                vm.advisors = data;
+                vm.isLoading = false;
+            });
+
+            var testKey = 'testKey';
+            var testData = {jon:'snow'};
+            dataCacheLocalStorage.put(testKey, testData);
+            dataCacheLocalStorage.remove(testKey);
+            console.log('remove: ', dataCacheLocalStorage.remove);
+            console.log('after removal: ', dataCacheLocalStorage.get(testKey));
+        }
+
+
+
+        init();
 
     }
 
