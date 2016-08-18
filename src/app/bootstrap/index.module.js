10e99b7075fb0d849afc156eab1bb77df9e25dec
Chong, Michael <michong@ci.com>
2016-08-18 14:16:41 -0400

    wip

--- src/app/bootstrap/index.module.js
+++ src/app/bootstrap/index.module.js
@@ -3,15 +3,15 @@
 
     //---------------- CORE ---------------- //
     angular.module('advisorLocator.core.cache', []);
-    angular.module('advisorLocator.core.server', ['advisorLocator.core.cache']);
-    angular.module('advisorLocator.core.main', ['advisorLocator.core.server', 'advisorLocator.utils']);
+    angular.module('advisorLocator.core.server', ['advisorLocator.core.cache', 'ui.bootstrap']);
+    angular.module('advisorLocator.core.main', ['advisorLocator.core.server', 'advisorLocator.utils', 'ui.bootstrap']);
     //---------------- FEATURES ---------------- //
-    angular.module('advisorLocator.features.searchByName', ['advisorLocator.core.server', 'advisorLocator.utils', 'ui.bootstrap', 'advisorLocator.features.service']);
-    angular.module('advisorLocator.features.searchByLocation', ['advisorLocator.core.server', 'advisorLocator.utils', 'advisorLocator.features.service']);
+    angular.module('advisorLocator.features.searchByName', ['advisorLocator.core.server', 'advisorLocator.utils', 'ui.bootstrap', 'advisorLocator.features.service', 'ui.bootstrap']);
+    angular.module('advisorLocator.features.searchByLocation', ['advisorLocator.core.server', 'advisorLocator.utils', 'advisorLocator.features.service', 'ui.bootstrap' ]);
     angular.module('advisorLocator.features.service', ['advisorLocator', 'advisorLocator.core.server', 'advisorLocator.utils', 'ui.bootstrap']);
 
     //---------------- UTILITIES ---------------- //
-    angular.module('advisorLocator.utils', ['advisorLocator.core.server', 'matchMedia', 'ng.deviceDetector']);
+    angular.module('advisorLocator.utils', ['advisorLocator.core.server', 'matchMedia', 'ng.deviceDetector', 'ui.bootstrap']);
 
     //---------------- MAIN ---------------- //
     angular.module('advisorLocator', [
--- src/app/utils/components/paginatorInfiniteScroll/paginatorInfiniteScroll.component.js
+++ src/app/utils/components/paginatorInfiniteScroll/paginatorInfiniteScroll.component.js
@@ -28,6 +28,26 @@
         vm.detectMobile = detectMobile;
 
 
+
+
+
+
+
+        vm.totalItems = 300;
+        vm.currentPage = 1;
+        vm.size = 10;
+        vm.pageChanged = function(x){
+            console.log('changing page: ', x);
+            vm.currentPage = x;
+        };
+
+        vm.pagination = {
+            currentPage: 1,
+            totalItems: 300,
+            itemsPerPage: 10,
+            maxSize:100
+        };
+
     }
 
 })();
