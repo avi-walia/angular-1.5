61eb8f9283f1d461f995a22279ddc86ff096361d
AWalia <awalia@ci.com>
2016-09-21 11:24:34 -0400

    CFA-327 Added the conditions to redirect the branch card to location search and the advisor card to name search respectively if they have been either reached directly or reloaded

--- src/app/bootstrap/index.run.js
+++ src/app/bootstrap/index.run.js
@@ -6,13 +6,13 @@
 
     runBlock.$inject = [
         '$rootScope', 'NotificationService',
-        '$translate', 'pageStateResolver', 'tmhDynamicLocale', '$window', 'stateTrackerService'
+        '$translate', 'pageStateResolver', 'tmhDynamicLocale', '$window', 'stateTrackerService','$state'
     ];
 
     /* @ngInject */
 
     function runBlock($rootScope, NotificationService,
-                      $translate, pageStateResolver, tmhDynamicLocale, $window, stateTrackerService) {
+                      $translate, pageStateResolver, tmhDynamicLocale, $window, stateTrackerService, $state) {
 
         // ionic stuff
         /*
@@ -80,6 +80,7 @@
                     if ('pageName' in oPageConfig) {
                         $rootScope.oRequestedPageConfig = oPageConfig;
                     }
+
                 });
 
                 /*
@@ -148,12 +149,27 @@
                 document.getElementsByTagName("head")[0].appendChild(scr);*/
                 ///////// *** END LANGUAGE NEGOTIATION *** /////////
 
+                if($rootScope.currentPage === $rootScope.nextPage && toState.name=='main.advisorLocator.advisorDetails'){
+                    event.preventDefault();
+                    $state.go('main.advisorLocator.advisorList');
+                }
+
+                if($rootScope.currentPage === $rootScope.nextPage && toState.name=='main.advisorLocator.branchDetails'){
+                    event.preventDefault();
+                    $state.go('main.advisorLocator.branchList');
+                }
+
                 pageStateResolver.pageLoading = false;
                 if (toState.resolve) {
                     pageStateResolver.pageLoading = true;
                 }
 
             });
+
+        $rootScope.$on("$locationChangeStart", function(event, next, current) {
+            $rootScope.currentPage = current;
+            $rootScope.nextPage = next;
+        });
 
     }//end: runBlock
 
