7aa18df273d010cb4112d1c8def50673a6b83598
Chong, Michael <michong@ci.com>
2016-10-04 15:31:09 -0400

    Possible fix for advisor name search.

--- src/app/features/components/advisorList/advisorList.service.js
+++ src/app/features/components/advisorList/advisorList.service.js
@@ -370,157 +370,116 @@
             A partial search term and a single character search term can still match on the same word. Will need maximum matching bipartite graph algorithm to solve that.
          */
         function termComparator(name, searchTerm, alreadyMatched) {
-/*
-            if (name === 'gianni' || name === 'de' || name === 'pastena') {
-                //console.log('name: ', name);
-                //console.log('searchTerm: ', searchTerm);
-                //console.log('alreadyMatched: ', alreadyMatched);
-                //console.log('searchTerm.length: ', searchTerm.length);
-            }
-*/
-/*
-            //if the searchTerm is only 1 letter long, it has to match against the first letter of the name.
+            var ret;
             if (searchTerm.length === 1) {
-                if (alreadyMatched.indexOf(name) < 0) {
-                    if (name.substring(0, 1) === searchTerm) {
-                        alreadyMatched.push(name);
-                        return true;
-                    } else {
-                        return false;
-                    }
-                } else {
-                    return true;
-                }
-            } else {
-                //console.log('ok', alreadyMatched);
-                if (alreadyMatched.indexOf(name) >= 0) {
-                    //console.log('huh?');
-                    return false;
-                } else {
-                    var temp = name.indexOf(searchTerm) >= 0;
-                    //console.log('temp: ', temp);
-                    if (temp) {
-                        alreadyMatched.push(name);
-                    }
-                    return temp;
-                }
-            }
-            */
+                ret = name.substring(0, 1) === searchTerm;
 
-            if (searchTerm.length === 1) {
-                return name.substring(0, 1) === searchTerm;
             } else {
-                return name.indexOf(searchTerm) >= 0;
+                ret = name.indexOf(searchTerm) >= 0;
             }
+
+            if (ret) {
+                alreadyMatched.push(searchTerm);
+            }
+            return ret;
         }
 
         function containsSearch(cNameArr, fNameArr, lNameArr, searchTerms, index) {
             var showCommon = false;
-
-            if (cNameArr[0] === 'gianni' || lNameArr[0] === 'gianni' || lNameArr[1] === 'de' || lNameArr[1] === 'pastena') {
-                //console.log('test1123', cNameArr);
-                //console.log('test1123', lNameArr);
-                //console.log('test1123', fNameArr);
-            }
+            var matches = 0;
             var alreadyMatched = [];
-            var remainingSearchTerms = [];
             var partialMatch = false;
-            var numMatchedSearchTerms = 0;
-            _.forEach(searchTerms, function(searchTerm, searchIndex) {
-                _.forEach(lNameArr, function(lName){
-                    if (termComparator(lName, searchTerm, alreadyMatched)) {
-                        if (cNameArr[0] === 'gianni' || lNameArr[0] === 'gianni' || lNameArr[1] === 'de' || lNameArr[1] === 'pastena') {
-                            //console.log('lname: ', lName);
-                            //console.log('searchTerm: ', searchTerm);
-                            //console.log('alreadyMatched: ', alreadyMatched);
+            //31916
+            var debug = service.allAdvisors[index].id == 31916;
+            if (debug) {
+                console.log('debug enabled advisor: ', service.allAdvisors[index]);
+            }
+            _.forEach(searchTerms, function(searchTerm) {
+                _.forEach(lNameArr, function(name) {
+                    if (termComparator(name, searchTerm, alreadyMatched)) {
+                        if (debug) {
+                            console.log('name1: ', name);
+                            console.log('searchTerm1: ', searchTerm);
                         }
-
+                        matches++;
                         partialMatch = true;
-                        numMatchedSearchTerms++;
-                    } else {
-                        if (remainingSearchTerms.indexOf(searchTerm) < 0) {
-                            remainingSearchTerms.push(searchTerm);
-                        }
+                        return false;
                     }
                 });
             });
-
-            var countMatches = 0;
-
-
-            if (cNameArr[0] === 'dixie' || lNameArr[0] === 'dixie' || lNameArr[0] === 'allen') {
-                //console.log('numMatchedSearchTerms: ', numMatchedSearchTerms);
-                //console.log('remainingSearchTerms: ', remainingSearchTerms);
-                //console.log('remainingSearchTerms: ', searchTerms);
-            }
-            if (numMatchedSearchTerms === searchTerms.length) {
-                if (cNameArr[0] === 'gianni' || lNameArr[0] === 'gianni' || lNameArr[1] === 'de' || lNameArr[1] === 'pastena') {
-                    //console.log('huh1123');
+            if (matches === searchTerms.length) {
+                if (service.allAdvisors[index].commonName) {
+                    service.allAdvisors[index].showCommon = true;
+                } else {
+                    service.allAdvisors[index].showCommon = false;
                 }
-                service.allAdvisors[index].showCommon = false;
+                console.log('asdf4');
                 service.searchResults.push(service.allAdvisors[index]);
-                console.log('arg3');
                 return;
             }
-            if (remainingSearchTerms.length) {
-                _.forEach(remainingSearchTerms, function (searchTerm, searchIndex) {
-                    _.forEach(cNameArr, function (commonName) {
-                        if (termComparator(commonName, searchTerm, alreadyMatched)) {
-                            partialMatch = true;
-                            countMatches++;
-                            showCommon = true;
-                        }
-                    });
+
+
+            var searchTerms2 = _.filter(searchTerms, function(searchTerm) {
+                var ret = true;
+                _.forEach(alreadyMatched, function(matchedSearchTerm) {
+                    if (debug) {
+                        console.log('name2: ', name);
+                        console.log('searchTerm2: ', searchTerm);
+                    }
+                    if (searchTerm === matchedSearchTerm) {
+                        ret = false;
+                        return false;
+                    }
                 });
-                if (countMatches === remainingSearchTerms.length) {
-                    if (remainingSearchTerms.length !== 0) {
+                return ret;
+            });
+            var searchTerms3 = searchTerms2.slice();
+            var tempAlreadyMatched = alreadyMatched.slice();
+            var tempMatches = matches;
+            _.forEach(searchTerms2, function(searchTerm) {
+                _.forEach(cNameArr, function(name) {
+                    if (termComparator(name, searchTerm, tempAlreadyMatched)) {
+
+                        if (debug) {
+                            console.log('name3: ', name);
+                            console.log('searchTerm3: ', searchTerm);
+                        }
+                        tempMatches++;
                         service.allAdvisors[index].showCommon = true;
-                    } else {
-                        service.allAdvisors[index].showCommon = false;
+                        partialMatch = true;
+                        return false;
                     }
-
-                    if (cNameArr[0] === 'gianni' || lNameArr[0] === 'gianni' || lNameArr[1] === 'de' || lNameArr[1] === 'pastena') {
-                        //console.log('test1: ');
-                    }
-                    service.searchResults.push(service.allAdvisors[index]);
-                    console.log('arg2');
-                    return;
-                }
-                countMatches = 0;
-
-/*
-                if (cNameArr[0] === 'dixie' || lNameArr[0] === 'dixie' || lNameArr[0] === 'allen') {
-                    console.log('remainingSearchTerms: ', remainingSearchTerms);
-                    console.log('fNameArr: ', fNameArr);
-                    console.log('fNameArr: ', fNameArr);
-                }*/
-                _.forEach(remainingSearchTerms, function (searchTerm, searchIndex) {
-                    _.forEach(fNameArr, function (firstName, searchIndex) {
-
-                        if (termComparator(firstName, searchTerm, alreadyMatched)) {
-
-                            if (cNameArr[0] === 'dixie' || lNameArr[0] === 'dixie' || lNameArr[0] === 'allen') {
-                                console.log('alreadyMatched: ', alreadyMatched);
-                                console.log('firstName: ', firstName);
-                                console.log('searchTerm: ', searchTerm);
-                            }
-                            partialMatch = true;
-                            countMatches++;
-                        }
-                    });
                 });
-                if (countMatches === remainingSearchTerms.length) {
-                    service.allAdvisors[index].showCommon = false;
-                    service.searchResults.push(service.allAdvisors[index]);
-                    console.log('arg1');
-                    if (cNameArr[0] === 'gianni' || lNameArr[0] === 'gianni' || lNameArr[1] === 'de' || lNameArr[1] === 'pastena') {
-                        //console.log('test2: ');
-                    }
-                    return;
-                }
+            });
+            if (tempMatches === searchTerms.length) {
+                console.log('asdf2');
+                service.searchResults.push(service.allAdvisors[index]);
+                return;
             }
-            if (partialMatch) {
-                service.allAdvisors[index].showCommon = showCommon;
+
+            tempAlreadyMatched = alreadyMatched.slice();
+            tempMatches = matches;
+            _.forEach(searchTerms3, function(searchTerm) {
+                _.forEach(fNameArr, function(name) {
+                    if (termComparator(name, searchTerm, alreadyMatched)) {
+
+                        if (debug) {
+                            console.log('name: ', name);
+                            console.log('searchTerm: ', searchTerm);
+                        }
+                        tempMatches++;
+                        partialMatch = true;
+                        return false;
+                    }
+                });
+            });
+            if (tempMatches === searchTerms.length) {
+                service.allAdvisors[index].showCommon = false;
+                console.log('asdf3:', service.allAdvisors[index]);
+                service.searchResults.push(service.allAdvisors[index]);
+
+                return;
+            } else if (partialMatch) {
                 secondaryResults.push(service.allAdvisors[index]);
             }
         };
