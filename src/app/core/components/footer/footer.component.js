0df6929d769a449192d08bac289cc4b71c40387d
Roslik, Dziana <droslik@ci.com>
2016-10-05 14:40:58 -0400

    drupal footer

--- src/app/core/components/footer/footer.component.js
+++ src/app/core/components/footer/footer.component.js
@@ -13,15 +13,19 @@
     /* @ngInject */
 
     footerCtrl.$inject = [
+        '$rootScope',
         'COPYRIGHT_YEAR',
+        'ASSANTE_URL',
         'detectMobile'
     ];
     /* @ngInject */
-    function footerCtrl(COPYRIGHT_YEAR, detectMobile
+    function footerCtrl($rootScope, COPYRIGHT_YEAR, ASSANTE_URL, detectMobile
     ) {
         var vm = this;
         vm.year = COPYRIGHT_YEAR;
         vm.detectMobile = detectMobile;
+        vm.ASSANTE_URL = ASSANTE_URL;
+        vm.currentLanguage = $rootScope.documentLanguage;
     }
 
 })();
